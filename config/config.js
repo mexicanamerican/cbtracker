7941d8d2a7b3571d28554827eff2ebd1e628a66f
mexicanamerican <mexican.american@gmail.com>
2021-08-12 22:05:50 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,26 +1,6 @@
 const env = process.env.NODE_ENV || 'development';
 
 const config = {
-  development: {
-    server: {
-      port: process.env.PORT || 3000,
-      hostname: process.env.HOSTNAME || 'localhost',
-    },
-    database: {
-      url: 'mongodb://localhost/express-development',
-    },
-  },
-
-  test: {
-    server: {
-      port: process.env.PORT || 3100,
-      hostname: process.env.HOSTNAME || 'localhost',
-    },
-    database: {
-      url: 'mongodb://localhost/express-test',
-    },
-  },
-
   production: {
     server: {
       port: process.env.PORT || 443,
@@ -32,8 +12,6 @@ const config = {
   },
 };
 
-config[env].isDev = env === 'development';
-config[env].isTest = env === 'test';
 config[env].isProd = env === 'production';
 
 module.exports = config[env];
